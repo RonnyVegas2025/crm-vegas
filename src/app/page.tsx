276e4eed8ad599e5e05057c052747118509aed4f
RonnyVegas2025 <ronnyizidorio46@gmail.com>
2026-03-30 11:44:07 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,1 +1,10 @@
+import { redirect } from 'next/navigation'
+import { createClient } from '@/lib/supabase-server'
+
+export default async function Home() {
+  const supabase = createClient()
+  const { data: { user } } = await supabase.auth.getUser()
+  if (user) redirect('/pos-venda')
+  else redirect('/login')
+}
 
